7de59a70e56e36bdda5c0f2d9180025fe676af13
Arkadii Shapkin <aseshapkin@sberbank.ru>
2023-12-23 02:53:24 +0300

    Rewrite from docker solution to python package version

--- README.md
+++ README.md
@@ -1,12 +1,49 @@
-# GN format hook for pre-commit
+# gn-build-py
 
-[GN](https://gn.googlesource.com/gn/+/main/docs/reference.md#cmd_format) package for [pre-commit](http://pre-commit.com).
+A Python wrapper to provide a pip-installable [GN](https://gn.googlesource.com/gn/+/main/docs/reference.md#cmd_format) binary, designed for use with [pre-commit](http://pre-commit.com). This tool simplifies the process of installing and using GN for source code formatting in projects that utilize pre-commit hooks.
 
-## Using gn-format with pre-commit
+## Using gn-build-py as a Pre-commit Hook
+
+To use `gn-build-py` as a pre-commit hook for formatting GN files, add the following configuration to your `.pre-commit-config.yaml`:
 
 ```yaml
--   repo: https://github.com/KindDragon/pre-commit-gn-format.git
-    rev: main
+-   repo: https://github.com/KindDragon/gn-build-py.git
+    rev: "latest"
     hooks:
-    -   id: gn-format
+    -   id: gn-build-py
 ```
+
+This configuration ensures that the GN binary is available and used by pre-commit to format GN files in your repository.
+
+## Building and Publishing a New Package Version
+
+The `build_and_push.sh` script is used to automate the process of building, testing, and deploying new versions of `gn-build-py`. It handles generating the latest `setup.cfg`, building the package, running tests, committing changes, and tagging the release.
+
+### Prerequisites
+
+- Ensure you have `python`, `pip`, and `git` installed.
+- Install `tox` for running tests: `pip install tox`.
+
+### Steps to Build and Publish
+
+1. **Run the Script**: Execute `build_and_push.sh` from the root directory of the project.
+
+    ```bash
+    ./build_and_push.sh
+    ```
+
+2. **Automated Process**: The script will:
+    - Generate the latest `setup.cfg` with updated GN version.
+    - Build the package.
+    - Run tests using `tox`.
+    - Commit the changes to the repository.
+    - Tag the commit with the new version.
+    - Push the commit and tag to the repository.
+
+3. **Upload to PyPI** (Optional): If you wish to distribute the package via PyPI, use [twine](https://pypi.org/project/twine/) to upload the distributions:
+
+    ```bash
+    twine upload dist/*
+    ```
+
+    Ensure you have the necessary credentials for PyPI and have tested the package thoroughly.
---
+++ gen_setup_cfg.py
@@ -0,0 +1,104 @@
+from __future__ import annotations
+
+import configparser
+import re
+import urllib.request
+
+
+def fetch_details(url: str):
+    with urllib.request.urlopen(url) as response:
+        html = response.read().decode("utf-8")
+
+    sha_match = re.search(
+        r'SHA256</b></td>\s*<td class="user-select-all">([a-f0-9]{64})</td>', html
+    )
+    instance_id_match = re.search(
+        r'Instance ID</b></td>\s*<td class="user-select-all">([^<]+)</td>', html
+    )
+    git_revision_match = re.search(r"git_revision:([a-f0-9]{40})", html)
+
+    if not sha_match or not instance_id_match or not git_revision_match:
+        raise Exception("Details not found in the page.")
+
+    return {
+        "sha256": sha_match.group(1),
+        "instance_id": instance_id_match.group(1),
+        "git_revision": git_revision_match.group(1),
+    }
+
+
+def generate_download_scripts():
+    platforms = {
+        "linux-amd64": {
+            "binary": "gn",
+            "marker": 'sys_platform == "linux" and platform_machine == "x86_64"',
+        },
+        "linux-arm64": {
+            "binary": "gn",
+            "marker": 'sys_platform == "linux" and platform_machine == "aarch64"',
+        },
+        "mac-amd64": {
+            "binary": "gn",
+            "marker": 'sys_platform == "darwin" and platform_machine == "x86_64"',
+        },
+        "mac-arm64": {
+            "binary": "gn",
+            "marker": 'sys_platform == "darwin" and platform_machine == "arm64"',
+        },
+        "windows-amd64": {
+            "binary": "gn.exe",
+            "marker": 'sys_platform == "win32" and platform_machine == "AMD64"',
+        },
+    }
+
+    prev_git_revision = ""
+    download_scripts = [""]
+    for platform, details in platforms.items():
+        url = f"https://chrome-infra-packages.appspot.com/p/gn/gn/{platform}/+/latest"
+        fetched_details = fetch_details(url)
+
+        if prev_git_revision == "":
+            prev_git_revision = fetched_details["git_revision"]
+        elif fetched_details["git_revision"] != prev_git_revision:
+            raise Exception(
+                f"Version mismatch for {platform}: expected {prev_git_revision}, got {fetched_details['git_revision']}"
+            )
+
+        download_script = f"""
+[gn]
+group = gn-binary
+marker = {details["marker"]}
+url = https://chrome-infra-packages.appspot.com/dl/gn/gn/{platform}/+/{fetched_details['instance_id']}
+sha256 = {fetched_details['sha256']}
+extract = zip
+extract_path = {details["binary"]}
+"""
+        download_scripts.append(download_script.strip())
+
+    return prev_git_revision, "\n".join(download_scripts)
+
+
+def create_setup_cfg(version, download_scripts):
+    config = configparser.ConfigParser()
+    config.read("setup.cfg.template")
+
+    # Replace placeholders in the template
+    config["metadata"]["version"] = version
+
+    # Add the dynamically generated download_scripts section
+    config.add_section("setuptools_download")
+    config.set("setuptools_download", "download_scripts", download_scripts)
+
+    # Write the new setup.cfg
+    with open("setup.cfg", "w") as config_file:
+        config.write(config_file)
+
+
+def main():
+    git_revision, download_scripts = generate_download_scripts()
+    version = "0.0.1+" + git_revision[:12]
+    create_setup_cfg(version, download_scripts)
+
+
+if __name__ == "__main__":
+    main()
